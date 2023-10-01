1ac95bbc5b476d3c8db09166c6fe7a5fe5e42399
Bandana Prakash <146561559+bandanaprakash@users.noreply.github.com>
2023-10-01 09:38:00 +0530

    added bpro.css

---
+++ bpro.js
@@ -0,0 +1,7 @@
+//toggle icon navbar
+ menuIcon=document.querySelector('#menu-icon')
+navbar=document.querySelector('.navbar')
+menuIcon.addEventListener('click',()=>{
+    navbar.classList.toggle('v-responsive');
+})
+
